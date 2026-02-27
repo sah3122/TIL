0043cd6dd09d179f2d6956f657bc62ebe60a3a6a
dean <sah3122@naver.com>
2026-02-27 23:04:23 +0900

    coroutine 모듈 변경

--- coroutine/src/main/kotlin/Main.kt
+++ coroutine/src/main/kotlin/Main.kt
@@ -1,20 +1,12 @@
 package io.weverse
 
-import LoomDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExecutorCoroutineDispatcher
 import kotlinx.coroutines.asCoroutineDispatcher
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import java.util.concurrent.Executor
 import java.util.concurrent.Executors
+
 
 val LoomDispatcher = Executors
     .newVirtualThreadPerTaskExecutor()
     .asCoroutineDispatcher()
-
-
 
 fun main() {
     SequenceBuilder.run()
